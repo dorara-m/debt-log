966c6726e0f260b9756c990376bc8094ad91c3b3
dorara <satosinisemono@gmail.com>
2019-07-10 22:41:34 +0900

    add: reset.css

--- src/App.vue
+++ src/App.vue
@@ -211,6 +211,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import url(./assets/_reset.css);
 .app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
