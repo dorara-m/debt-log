8f91486dc6085e82e2779a8822ff9cb42ca48e43
dorara@work <satosinisemono@gmail.com>
2019-08-07 19:32:11 +0900

    config参照

--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import firebase from 'firebase/app'
 
-Vue.config.productionTip = false;
+import config from '../config/key';
 
 // Initialize Firebase
-
+const firebaseConfig = {
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  databaseURL: config.databaseURL,
+  projectId: config.projectId,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId,
+  appId: config.appId
+};
 firebase.initializeApp(firebaseConfig);
 
 new Vue({
